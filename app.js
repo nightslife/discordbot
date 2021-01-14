fa5a3f73913e7d96029286e3b9cceb5880d0a268
nightslife <ragefail1212@gmail.com>
2021-01-13 23:15:18 -0500

    Updated to DoG server standards

--- app.js
+++ app.js
@@ -9,17 +9,19 @@ let forwardOfficerID = process.env.officerID;
 
 function pasteToOfficer(message) {
     let user = message.member.user;
-    if(message.member.roles.cache.find(role => role.name === 'officers')){
+    if(message.member.roles.cache.find(role => role.name === 'Officer')){
         return
     }
     let officerChannel = message.guild.channels.cache.find(ch => ch.name === process.env.application_channel)
-    let officerRole = message.channel.guild.roles.cache.find(role => role.name === 'officers');
+    let officerRole = message.channel.guild.roles.cache.find(role => role.name === 'Officer');
     const embed = new Discord.MessageEmbed()
         .setTitle(`${user.username}#${user.discriminator}`)
         .setDescription(message.content);
     officerChannel.send("<@&"+officerRole.id+"> New Recruit",{embed:embed})
     user.createDM().then((channel) => {
-        channel.send(`Hello ${user.username}`);
+        channel.send(`Hello ${user.username}. Thank you for your interest in joining Delusions of Grandeur - Zul'Jin. Your application has been moved to the review channel and the officers have been notified. 
+        Below is a copy of the application that was sent. If you wish to edit, update, or just want to reach out to the officers, please reply to me instead of submitting another message in our discord. Thank you again 
+        for your interest and good luck.`);
         channel.send({embed: embed});
     }).catch((err) =>{
         console.log(err)
