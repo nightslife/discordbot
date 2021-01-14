c98d09abbc07e610abe24cc158d8fce1b9eb9a9a
nightslife <ragefail1212@gmail.com>
2021-01-13 21:57:11 -0500

    Full working bot

--- app.js
+++ app.js
@@ -1,73 +1,51 @@
 const Discord = require('discord.js');
+const config = require("./config.json")
 const client = new Discord.Client();
 
+const clientBotId = config.BOT_TOKEN
+const applicationMessagedeleteTime = 1;
 
-const clientBotId = ;//<<insert a discord bot client id here>>;
-const guildId = ;//discord server ID
-const applicationChannelId = '790983281877057586'; //application channel ID
-const officerChannelId = '219584056289918976'; //officer channel ID
-const officerRoleId = '<@200357405119217684>'; // officer role ID after the @ sign
-const officerForForwardingId = '205135006585061376'; //<id of the officer you want to bot to message> ;
-const applicationMessagedeleteTime = 10;
+let forwardOfficerID = config.officerID;
 
-let officerForForwarding;
-let officerChannel;
-let officerRole;
-
-function pasteApplicationToOfficerChannel(message) {
-  // Get the current user.
-  let user = message.member.user;
-
-  // Copy their message message into the officer channel using a rich text format for easy reading.
-  const embed = new Discord.RichEmbed()
-    .setTitle(`${user.username}#${user.discriminator}`)
-    .setColor(0x00AE86)
-    .setDescription(message.content);
-  officerChannel.send({embed});
-
-  // Notify the officers of the new message.
-  officerChannel.send(officerRoleId.toString()+"New Recruit");
-
-  // DM the applicant and let them know their message was received.
-  user.createDM().then((channel) => {
-    channel.send(`Hello ${user.username}, and thank you for your interest in joining <Delusions of Grandeur> - Runetotem! The officers have received your application, will review it and get back to you with a response as soon as possible. For your privacy, and ours, your application has been moved to an officer-only channel and deleted from public view. A copy of your application is below, and if you wish to edit or add to it, just reply here instead of making a new app and I will ensure the officers see it. Thanks again, and good luck =D !`);
-	channel.send({embed});
-  }).catch((err) => {
-    console.log(err);
-  });
-
-  // Delete the message after a set time.
-  message.delete(applicationMessagedeleteTime);
+function pasteToOfficer(message) {
+    let user = message.member.user;
+    if(message.member.roles.cache.find(role => role.name === 'Officer')){
+        return
+    }
+    let officerChannel = message.guild.channels.cache.find(ch => ch.name === config.application_channel)
+    let officerRole = message.channel.guild.roles.cache.find(role => role.name === 'Officer');
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`${user.username}#${user.discriminator}`)
+        .setDescription(message.content);
+    officerChannel.send("<@&"+officerRole.id+"> New Recruit",{embed:embed})
+    user.createDM().then((channel) => {
+        channel.send(`Hello ${user.username}`);
+        channel.send({embed: embed});
+    }).catch((err) =>{
+        console.log(err)
+    })
+    message.delete({timeout: applicationMessagedeleteTime})
 }
 
-function relayDMsToOfficer(message) {
-  // Do not forward messages that the bot is sending out
-  if (client.user.id === message.author.id) return;
-  let user = message.author;
-  // Forward the message to the officer in charge
-  officerForForwarding.createDM().then((channel) => {
-    // Copy their message message into the DM using a rich text format for easy reading.
-    const embed = new Discord.RichEmbed()
-      .setTitle(`${user.username}#${user.discriminator}`)
-      .setColor(0x00AE86)
-      .setDescription(message.content);
-    channel.send({embed});
-  }).catch((err) => {
-    console.log(err);
-  });
-}
 
+function relayDMs(message) {
+    let user = message.author;
+    if(client.user.id === message.author.id)return
+    const embed = new Discord.MessageEmbed()
+        .setTitle(`${user.username}#${user.discriminator}`)
+        .setDescription(message.content);
+    client.users.cache.get(forwardOfficerID).send({embed: embed}).catch((err) => {console.log(err)})
+
+}
 client.on('ready', () => {
-  officerRole = Array.from(client.guilds.get(guildId).roles.filter(role => role.name == 'Officer'))[0][1];
-  console.log('I am ready!');
-  officerChannel = client.channels.get(officerChannelId);
-  officerForForwarding = client.guilds.get(guildId).members.get(officerForForwardingId).user;
+    console.log('I am ready');
 });
 
 client.on('message', message => {
-  if (!message.channel) return;
-  if (message.channel.id === applicationChannelId) pasteApplicationToOfficerChannel(message);
-  if (message.channel.type === 'dm') relayDMsToOfficer(message);
+    if (!message.channel) return;
+    // console.log(message)
+    if (message.channel.id === config.applicationChannelID) pasteToOfficer(message)
+    if (message.channel.type === 'dm') relayDMs(message)
 });
 
-client.login(clientBotId);
+client.login(clientBotId)
